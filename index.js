8e354a1b268edf85dd93493a912dfe7ccbd11385
Charlie <sonthayaphuree@gmail.com>
2026-02-18 01:20:49 +0700

    Add logic of the game

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () =>{
-    return Math.floor(Math.random() * 3) +1;
-    
+    let computerChoice = Math.floor(Math.random() * 3) +1;
+    switch (computerChoice){
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+        default:
+    }
+    return computerChoice;
+
 };
 
-console.log(getComputerChoice());
-
-let getHumanChoice = (humanChoice) =>{
-
+let getHumanChoice = () =>{
+    let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    let upperCase = humanChoice.charAt(0).toUpperCase();
+    humanChoice = upperCase+ humanChoice.slice(1);
+    return humanChoice;
 }
 
-let humanChoice = prompt("Rock, Paper or Scissors?").toLocaleLowerCase();
+let playRound = (humanChoice, computerChoice) => {
+    if(humanChoice == "Rock" && computerChoice == "Scissors"){
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    }else if(humanChoice == "Rock" && computerChoice == "Paper"){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore +=1;
+    }else if(humanChoice == "Rock" && computerChoice == "Rock") {
+        console.log(`You Tie ${humanChoice} tie with ${computerChoice}`);
+    }else if(humanChoice == "Paper" && computerChoice == "Rock"){
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    }else if(humanChoice == "Paper" && computerChoice == "Scissors"){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore +=1;
+    }else if(humanChoice == "Paper" && computerChoice == "Paper"){
+        console.log(`You tie ${humanChoice} tie with ${computerChoice}`);
+    }else if(humanChoice == "Scissors" && computerChoice == "Paper"){
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+        humanScore +=1;
+    }else if(humanChoice == "Scissors" && computerChoice == "Rock"){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore +=1;
+    }else{
+        console.log(`You tie ${humanChoice} tie with ${computerChoice}`);
+    }
+}
 
-console.log(humanChoice);
+let whoLead = (humanScore, computerScore) => {
+    if(humanScore > computerScore){
+        console.log(`Human is a leader with ${humanScore} while Computer score = ${computerScore}`);
+    }else if(computerScore > humanScore){
+        console.log(`Computer is leading with ${computerScore} while Human score = ${humanScore}`);
+    }else {
+        console.log(`It's a tie with both score at: ${humanScore}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+whoLead(humanScore, computerScore);
+
